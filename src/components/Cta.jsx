e3519069344445ed84b2b0a2c020dcb7db85f027
Pabo Bernando <pabobernando69@gmail.com>
2023-09-25 23:29:34 +0700

    change content

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -7,26 +7,26 @@ export default function Cta() {
      <div>
 
 
-<div className="relative z-20 flex items-center overflow-hidden bg-white dark:bg-gray-800">
+<div className="relative z-20 flex items-center overflow-hidden bg-white">
     <div className="container relative flex px-6 py-16 mx-auto">
         <div className="relative z-20 flex flex-col sm:w-2/3 lg:w-2/5">
-            <span className="w-20 h-2 mb-12 bg-gray-800 dark:bg-white">
+            <span className="w-20 h-2 mb-12 bg-gray-800">
             </span>
-            <h1 className="flex flex-col text-5xl font-black leading-none text-gray-800 uppercase font-bebas-neue sm:text-6xl dark:text-white">
+            <h1 className="flex flex-col text-5xl font-black leading-none text-gray-800 uppercase font-bebas-neue sm:text-6xl">
             Temukan Kemudahan Perjalanan
                 <span className="text-5xl sm:text-6xl">
                     Bersama Kami
                 </span>
             </h1>
-            <p className="text-sm text-gray-700 sm:text-base dark:text-white">
+            <p className="text-sm text-gray-700 sm:text-base">
             Kami hadir untuk menjadikan setiap momen perjalanan Anda lebih nyaman dan memuaskan. Dengan armada mobil terbaru dan layanan terbaik kami, kami siap membantu Anda menjelajahi tujuan impian Anda dengan mudah
             </p>
             <div className="flex mt-8">
                 <Link to="/product" className="px-4 py-2 mr-4 text-white uppercase bg-blue-500 border-2 border-transparent rounded-lg text-md hover:bg-blue-400">
                     Cek Harga
                 </Link>
-                <Link to="/" className="px-4 py-2 text-blue-500 uppercase bg-transparent border-2 border-blue-500 rounded-lg dark:text-white hover:bg-blue-500 hover:text-white text-md">
-                    Pesan Sekarang
+                <Link to="/travel" className="px-4 py-2 text-blue-500 uppercase bg-transparent border-2 border-blue-500 rounded-lg hover:bg-blue-500 hover:text-white text-md">
+                    Rekomendasi Wisata
                 </Link>
             </div>
         </div>
@@ -35,8 +35,6 @@ export default function Cta() {
         </div>
     </div>
 </div>
-
-
 
       </div>
     );
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -5,16 +5,16 @@ function Section1() {
   return (
     <div>
 
-<div className="container p-6 px-6 mx-auto bg-white dark:bg-gray-800">
+<div className="container p-6 px-6 mx-auto bg-white ">
     <div className="mb-16 text-center">
         <h2 className="text-base font-semibold tracking-wide text-indigo-600 uppercase">
             Ghany Tour Travel
         </h2>
-        <p className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+        <p className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900">
             Mengapa sewa mobil di Ghany ?
         </p>
     </div>
-    <div className="flex flex-wrap my-12 dark:text-white">
+    <div className="flex flex-wrap my-12">
         <div className="w-full p-8 border-b md:w-1/2 md:border-r lg:w-1/3">
             <div className="flex items-center mb-6">
                 <CheckBadgeIcon className='w-6 h-6 text-pink-500' />
@@ -22,7 +22,7 @@ function Section1() {
                 Kualitas Armada
                 </div>
             </div>
-            <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
+            <p className="leading-loose text-gray-500 text-md">
             Setiap mobil dalam armada kami dipilih secara cermat dan dirawat dengan hati-hati untuk memastikan bahwa mereka selalu berada dalam kondisi terbaik. Kami menjamin bahwa setiap kendaraan yang Anda sewa dari kami akan memberikan kenyamanan dan keamanan yang tak tertandingi selama perjalanan Anda.
             </p>
         </div>
@@ -33,7 +33,7 @@ function Section1() {
                 Harga yang Bersaing
                 </div>
             </div>
-            <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
+            <p className="leading-loose text-gray-500  text-md">
             Kami percaya bahwa perjalanan haruslah sesuatu yang dapat diakses oleh semua orang, dan harga yang bersaing adalah salah satu cara kami mewujudkannya. Jadilah bagian dari komunitas pelanggan kami yang puas dan nikmati perjalanan hemat tanpa mengorbankan kualitas.
             </p>
         </div>
@@ -44,7 +44,7 @@ function Section1() {
                 Jam Operasional Fleksibel
                 </div>
             </div>
-            <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
+            <p className="leading-loose text-gray-500  text-md">
             Sepenuh hati mengerti bahwa perjalanan tidak selalu mengikuti jadwal tetap. Oleh karena itu, kami dengan senang hati menawarkan jam operasional yang fleksibel untuk memberikan Anda kenyamanan dan aksesibilitas sepanjang waktu.
             </p>
         </div>
@@ -55,7 +55,7 @@ function Section1() {
                 Keamanan dan Kebersihan
                 </div>
             </div>
-            <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
+            <p className="leading-loose text-gray-500  text-md">
             Keamanan dan kebersihan adalah komitmen utama kami. Kami mengerti bahwa dalam dunia yang terus berubah, pelanggan kami mencari kenyamanan dan perlindungan ekstra selama perjalanan mereka. Itulah mengapa kami menjadikan keamanan dan kebersihan sebagai prioritas tertinggi dalam setiap aspek layanan kami.
             </p>
         </div>
@@ -66,7 +66,7 @@ function Section1() {
                 Proses Pemesanan yang Mudah
                 </div>
             </div>
-            <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
+            <p className="leading-loose text-gray-500  text-md">
             Memahami bahwa waktu Anda sangat berharga. Oleh karena itu, kami telah merancang proses pemesanan yang sederhana dan cepat sehingga Anda dapat mulai petualangan Anda hanya dalam sekejap mata.
             </p>
         </div>
@@ -77,7 +77,7 @@ function Section1() {
                 Pelayanan Pelanggan yang Ramah
                 </div>
             </div>
-            <p className="leading-loose text-gray-500 dark:text-gray-200 text-md">
+            <p className="leading-loose text-gray-500  text-md">
             Bukan hanya penyedia mobil, tetapi juga partner perjalanan Anda yang selalu siap membantu dan memberikan perhatian penuh kepada kebutuhan Anda. Kami memahami bahwa perjalanan Anda adalah pengalaman pribadi, dan kami siap membuatnya lebih mudah dan lebih menyenangkan.
             </p>
         </div>
